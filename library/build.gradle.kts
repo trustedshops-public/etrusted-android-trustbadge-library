90ac9d8d76be4e61f5a5b33d6008aa50c3fdb70e
Ali Kabiri <k4biri@icloud.com>
2023-01-24 16:31:25 +0100

    create a gradle plugin to copy library properties; update dependencies
    Signed-off-by: Ali Kabiri <k4biri@icloud.com>

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,23 +1,56 @@
+import org.json.JSONObject
+import java.io.FileInputStream
+import java.util.*
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
 }
 
-abstract class TrustbadgeGradlePlugin: Plugin<Project> {
+class TrustbadgeGradlePlugin: Plugin<Project> {
+
+    private val propFileName = "trustbadge.properties"
+    private val jsonFileName = "trustbadge-config.json"
+    private val keyClientId = "client_id"
+    private val keyClientSecret = "client_secret"
+
+    private fun decodeJsonAndProduceConfigFile(
+        inputPath: String,
+        outputPath: String,
+    ) {
+
+        println("decoding json")
+        val jsonContent = File(inputPath).readText()
+        val jsonObject = JSONObject(jsonContent)
+
+        val clientId = jsonObject.get(keyClientId)
+        val clientSecret = jsonObject.get(keyClientSecret)
+
+        println("generating $propFileName file")
+        val propContent = "$keyClientId=$clientId\n$keyClientSecret=$clientSecret"
+        val outputFile = File(outputPath)
+        outputFile.createNewFile()
+        outputFile.writeText(propContent)
+    }
 
     override fun apply(project: Project) {
-        project.task("produceConfiguration") {
+        project.tasks.register("produceConfig") {
             doLast {
                 project.copy {
-                    from("${project.rootDir}/app/trustbadge-config.json")
-                    into("${project.buildDir}/generated/res/values")
+                    from("${project.rootDir}/$jsonFileName")
+                    into("${project.projectDir}")
                 }
+                decodeJsonAndProduceConfigFile(
+                    inputPath = "${project.projectDir}/$jsonFileName",
+                    outputPath = "${project.projectDir}/$propFileName"
+                )
             }
         }
     }
 }
 
 apply<TrustbadgeGradlePlugin>()
+tasks.findByName("produceConfig")?.finalizedBy(tasks.assemble)
 
 android {
     namespace = "com.etrusted.android.trustbadge.library"
@@ -31,9 +64,42 @@ android {
     }
 
     buildTypes {
+
+        val propFileName = "trustbadge.properties"
+        val keyClientId = "client_id"
+        val keyClientSecret = "client_secret"
+
         named("release") {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+
+            // create an empty config file to avoid failing builds
+            File("${project.projectDir}/$propFileName").apply {
+                createNewFile(); writeText("$keyClientId=\n$keyClientSecret=") }
+
+            // load properties
+            val propertiesFile = project.file(propFileName)
+            val properties = Properties()
+            properties.load(FileInputStream(propertiesFile))
+
+            // produce rest values available to the library
+            resValue("string", keyClientId, properties.getProperty(keyClientId))
+            resValue("string", keyClientSecret, properties.getProperty(keyClientSecret))
+        }
+        named("debug") {
+
+            // create an empty config file to avoid failing builds
+            File("${project.projectDir}/$propFileName").apply {
+                createNewFile(); writeText("$keyClientId=\n$keyClientSecret=") }
+
+            // load properties
+            val propertiesFile = project.file(propFileName)
+            val properties = Properties()
+            properties.load(FileInputStream(propertiesFile))
+
+            // produce rest values available to the library
+            resValue("string", keyClientId, properties.getProperty(keyClientId))
+            resValue("string", keyClientSecret, properties.getProperty(keyClientSecret))
         }
     }
     compileOptions {
@@ -48,19 +114,23 @@ android {
     }
 }
 
-internal val activity_version: String by project
-internal val compose_version: String by project
-internal val compose_m3_version: String by project
+internal val coreKtxVersion: String by project
+internal val activityVersion: String by project
+internal val composeVersion: String by project
+internal val composeM3Version: String by project
+internal val testJunitVersion: String by project
+internal val androidTestJunitVersion: String by project
+internal val androidTestEspressoVersion: String by project
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.activity:activity-compose:$activity_version")
-    implementation("androidx.compose.ui:ui:$compose_version")
-    debugImplementation("androidx.compose.ui:ui-tooling:$compose_version")
-    implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
-    implementation("androidx.compose.material3:material3:$compose_m3_version")
+    implementation("androidx.core:core-ktx:$coreKtxVersion")
+    implementation("androidx.activity:activity-compose:$activityVersion")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.compose.material3:material3:$composeM3Version")
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation("junit:junit:$testJunitVersion")
+    androidTestImplementation("androidx.test.ext:junit:$androidTestJunitVersion")
+    androidTestImplementation("androidx.test.espresso:espresso-core:$androidTestEspressoVersion")
 }
