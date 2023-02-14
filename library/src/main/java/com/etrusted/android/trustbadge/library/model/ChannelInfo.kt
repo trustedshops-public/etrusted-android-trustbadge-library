52b2d1891eb1ac05f44203d292452844200f204b
Ali Kabiri <k4biri@icloud.com>
2023-02-14 17:32:59 +0100

    Create ChannelInfo.kt
    Signed-off-by: Ali Kabiri <k4biri@icloud.com>

---
+++ library/src/main/java/com/etrusted/android/trustbadge/library/model/ChannelInfo.kt
@@ -0,0 +1,113 @@
+/*
+ * Created by Ali Kabiri on 30.1.2023.
+ * Copyright (c) 2023 Trusted Shops GmbH
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.etrusted.android.trustbadge.library.model
+
+import org.json.JSONObject
+import java.util.Date
+
+internal data class ChannelInfo(
+
+    var week: AggregateRating,
+    var month: AggregateRating,
+    var quarter: AggregateRating,
+    var year: AggregateRating,
+    var overall: AggregateRating
+) {
+    data class AggregateRating(
+        var count: Int,
+        var rating: Float,
+        var distribution: AggregateRatingDistribution? = null,
+        var period: AggregateRatingPeriod? = null,
+    ) {
+        data class AggregateRatingDistribution(
+            var oneStar: Int,
+            var twoStars: Int,
+            var threeStars: Int,
+            var fourStars: Int,
+            var fiveStars: Int
+        )
+        data class AggregateRatingPeriod(
+            var start: Date,
+            var end: Date,
+            var firstConsideredReviewSubmission: Date?,
+            var lastConsideredReviewSubmission: Date?,
+            var calculatedAt: Date?,
+            var ratingTrend: RatingTrend?
+        ) {
+            enum class RatingTrend { NEGATIVE, NEUTRAL, POSITIVE }
+        }
+    }
+
+    companion object {
+        private const val KEY_7DAYS = "7days"
+        private const val KEY_30DAYS = "30days"
+        private const val KEY_90DAYS = "90days"
+        private const val KEY_365DAYS = "365days"
+        private const val KEY_OVERALL = "overall"
+        private const val KEY_COUNT = "count"
+        private const val KEY_RATING = "rating"
+
+
+        fun fromString(body: String): ChannelInfo {
+            val response7days = JSONObject(body).getJSONObject(KEY_7DAYS)
+            val response30days = JSONObject(body).getJSONObject(KEY_30DAYS)
+            val response90days = JSONObject(body).getJSONObject(KEY_90DAYS)
+            val response365days = JSONObject(body).getJSONObject(KEY_365DAYS)
+            val responseOverall = JSONObject(body).getJSONObject(KEY_OVERALL)
+
+            return ChannelInfo(
+                week = AggregateRating(
+                    count = response7days.getInt(KEY_COUNT),
+                    rating = response7days.getDouble(KEY_RATING).toFloat()
+                ),
+                month = AggregateRating(
+                    count = response30days.getInt(KEY_COUNT),
+                    rating = response30days.getDouble(KEY_RATING).toFloat()
+                ),
+                quarter = AggregateRating(
+                    count = response90days.getInt(KEY_COUNT),
+                    rating = response90days.getDouble(KEY_RATING).toFloat()
+                ),
+                year = AggregateRating(
+                    count = response365days.getInt(KEY_COUNT),
+                    rating = response365days.getDouble(KEY_RATING).toFloat()
+                ),
+                overall = AggregateRating(
+                    count = responseOverall.getInt(KEY_COUNT),
+                    rating = responseOverall.getDouble(KEY_RATING).toFloat()
+                ),
+            )
+        }
+    }
+}
+
+internal fun ChannelInfo.enrichTrustbadgeDataWithInfo(data: TrustbadgeData): TrustbadgeData {
+    return data.copy(
+        shop = data.shop.copy(
+            rating = this.year.rating
+        )
+    )
+}
