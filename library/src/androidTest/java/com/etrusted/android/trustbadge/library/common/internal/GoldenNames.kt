aa9dd430e9f25905705ddfdb850df3d46ad375b5
Ali Kabiri <k4biri@icloud.com>
2023-02-27 13:19:47 +0100

    add GoldenNames to hold expected-output-file names in one place
    Signed-off-by: Ali Kabiri <k4biri@icloud.com>

---
+++ library/src/androidTest/java/com/etrusted/android/trustbadge/library/common/internal/GoldenNames.kt
@@ -0,0 +1,36 @@
+/*
+ * Created by Ali Kabiri on 27.2.2023.
+ * Copyright (c) 2023 Trusted Shops GmbH
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.etrusted.android.trustbadge.library.common.internal
+
+/**
+ * A golden file is an expected output file
+ * The GoldenNames are the expected file names for screenshots stored in the assets folder.
+ *
+ *  @param raw the file name of an expected screenshot
+ */
+internal sealed class GoldenNames(val raw: String) {
+    object GoldenTrustbadgeDefaultUncertified: GoldenNames("screenshot-trustbadge-uncertified-default")
+}
