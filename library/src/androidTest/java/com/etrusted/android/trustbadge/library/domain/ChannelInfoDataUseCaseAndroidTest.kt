4b77cb0a3050ea77a9980005c91918388600d626
Ali Kabiri <k4biri@icloud.com>
2023-03-29 17:59:23 +0200

    architecture: separate ChannelInfo and TrustbadgeData use cases in domain layer
    Signed-off-by: Ali Kabiri <k4biri@icloud.com>

---
+++ library/src/androidTest/java/com/etrusted/android/trustbadge/library/domain/ChannelInfoDataUseCaseAndroidTest.kt
@@ -0,0 +1,104 @@
+/*
+ * Created by Ali Kabiri on 27.3.2023.
+ * Copyright (c) 2023 Trusted Shops GmbH
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.etrusted.android.trustbadge.library.domain
+
+import com.etrusted.android.trustbadge.library.common.internal.getFakeChannelInfoDataUseCase
+import com.etrusted.android.trustbadge.library.common.internal.getFakeTrustbadgeRepository
+import com.etrusted.android.trustbadge.library.domain.GetTrustbadgeDataUseCase.*
+import com.etrusted.android.trustbadge.library.model.TrustbadgeData
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class TrustbadgeDataUseCaseAndroidTest {
+
+    @Test
+    fun testTrustbadgeDataUseCaseReturnsSuccessfully() = runTest {
+
+        // arrange
+        val fakeTrustbadgeRepo = getFakeTrustbadgeRepository()
+        val fakeChannelInfoDataUseCase = getFakeChannelInfoDataUseCase()
+        val sut = GetTrustbadgeDataUseCase(
+            trustbadgeRepository = fakeTrustbadgeRepo,
+            getChannelInfoDataUseCase = fakeChannelInfoDataUseCase,
+        )
+
+        // act
+        val result = sut("fakeString", "fakeString")
+        advanceUntilIdle()
+
+        // assert
+        assertThat(result.isSuccess).isTrue()
+        assertThat(result.getOrNull()).apply {
+            isNotNull()
+            isInstanceOf(TrustbadgeData::class.java)
+        }
+    }
+
+    @Test
+    fun testTrustbadgeDataUseCaseFailsCorrectly() = runTest {
+
+        // arrange
+        val fakeTrustbadgeRepo = getFakeTrustbadgeRepository(Result.failure(Throwable("failed")))
+        val fakeChannelInfoDataUseCase = getFakeChannelInfoDataUseCase()
+        val sut = GetTrustbadgeDataUseCase(
+            trustbadgeRepository = fakeTrustbadgeRepo,
+            getChannelInfoDataUseCase = fakeChannelInfoDataUseCase,
+        )
+
+        // act
+        val result = sut("fakeString", "fakeString")
+        advanceUntilIdle()
+
+        // assert
+        assertThat(result.isFailure).isTrue()
+        assertThat(result.getOrNull()).isNull()
+    }
+
+    @Test
+    fun testTrustbadgeDataUseCaseFailsCorrectly2() = runTest {
+
+        // arrange
+        val fakeTrustbadgeRepo = getFakeTrustbadgeRepository(Result.failure(Throwable("failed")))
+        val fakeChannelInfoDataUseCase = getFakeChannelInfoDataUseCase()
+        val sut = GetTrustbadgeDataUseCase(
+            trustbadgeRepository = fakeTrustbadgeRepo,
+            getChannelInfoDataUseCase = fakeChannelInfoDataUseCase,
+        )
+
+        // act
+        val result = sut.invoke("fakeString", "fakeString")
+        advanceUntilIdle()
+
+        // assert
+        assertThat(result.isFailure).isTrue()
+        assertThat(result.getOrNull()).isNull()
+    }
+
+}
---
+++ library/src/main/java/com/etrusted/android/trustbadge/library/data/repository/ChannelInfoRepository.kt
@@ -0,0 +1,72 @@
+/*
+ * Created by Ali Kabiri on 30.1.2023.
+ * Copyright (c) 2023 Trusted Shops GmbH
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.etrusted.android.trustbadge.library.data.repository
+
+import com.etrusted.android.trustbadge.library.data.datasource.IShopGradeDetailDatasource
+import com.etrusted.android.trustbadge.library.data.datasource.ITrustbadgeDatasource
+import com.etrusted.android.trustbadge.library.data.datasource.ShopGradeDetailDatasource
+import com.etrusted.android.trustbadge.library.data.datasource.TrustbadgeDatasource
+import com.etrusted.android.trustbadge.library.model.AuthenticationToken
+import com.etrusted.android.trustbadge.library.model.TrustbadgeData
+import com.etrusted.android.trustbadge.library.model.enrichTrustbadgeDataWithInfo
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+internal interface ITrustbadgeRepository {
+    suspend fun fetchTrustbadgeData(tsid: String, channelId: String): Result<TrustbadgeData>
+}
+
+/**
+ * This class aims on reading the Trustbadge data
+ * without using any third party library (e.g. Okhttp, Retrofit, etc...)
+ */
+internal class TrustbadgeRepository
+constructor(
+    private val token: AuthenticationToken,
+    private val trustbadgeDatasource: ITrustbadgeDatasource = TrustbadgeDatasource(),
+    private val shopGradeDetailDatasource: IShopGradeDetailDatasource = ShopGradeDetailDatasource(),
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
+): ITrustbadgeRepository {
+
+    @Throws
+    override suspend fun fetchTrustbadgeData(
+        tsid: String,
+        channelId: String,
+    ): Result<TrustbadgeData> {
+
+        return withContext(dispatcher) {
+
+            val channelInfoData = shopGradeDetailDatasource.fetchShopGradeDetail(
+                    channelId = channelId,
+                    accessToken = token.accessToken).getOrThrow()
+            val tBadgeData = trustbadgeDatasource.fetchTrustbadge(tsid).getOrThrow()
+            val tBadgeWithRating = channelInfoData.enrichTrustbadgeDataWithInfo(tBadgeData)
+
+            Result.success(tBadgeWithRating)
+        }
+    }
+}
