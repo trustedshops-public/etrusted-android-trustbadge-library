a0db59f63bcd73fe384096bff436304c3c109fe9
Ali Kabiri <k4biri@icloud.com>
2023-05-16 15:22:29 +0200

    feature: hide trustbadge on scroll; show again when the scroll is back to zero
    Signed-off-by: Ali Kabiri <k4biri@icloud.com>

--- app/src/main/java/com/etrusted/android/trustbadgeexample/ui/common/TrustbadgeHelper.kt
+++ app/src/main/java/com/etrusted/android/trustbadgeexample/ui/common/TrustbadgeHelper.kt
@@ -1,2 +1,30 @@
 package com.etrusted.android.trustbadgeexample.ui.common
 
+import androidx.compose.foundation.ScrollState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import com.etrusted.android.trustbadge.library.ui.badge.TrustbadgeState
+import kotlinx.coroutines.delay
+
+/**
+ * Hides the badge after a the scroll state is not in position-zero
+ * Applies a short delay before hiding the badge
+ * Shows the badge when the scroll state is back in position-zero
+ *
+ * @param scrollState scroll state which the badge needs to react to
+ * @param badgeState corresponding badge state
+ */
+@Composable
+internal fun HideBadgeOnScroll(
+    scrollState: ScrollState,
+    badgeState: TrustbadgeState
+) {
+    if (scrollState.value == 0) {
+        badgeState.show()
+    } else {
+        LaunchedEffect(null) {
+            delay(1000)
+            badgeState.hide()
+        }
+    }
+}
