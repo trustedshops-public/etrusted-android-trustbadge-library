ebd8457adebe5a797fe0ad594614044e6a50c7be
Ali Kabiri <k4biri@icloud.com>
2023-09-22 11:37:33 +0200

    feat: add TrustcardProtection and its resources
    Signed-off-by: Ali Kabiri <k4biri@icloud.com>

---
+++ library/src/main/java/com/etrusted/android/trustbadge/library/ui/card/TrustcardProtection.kt
@@ -0,0 +1,161 @@
+/*
+ * Created by Ali Kabiri on 22.9.2023.
+ * Copyright (c) 2023 Trusted Shops AG
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.etrusted.android.trustbadge.library.ui.card
+
+import android.content.res.Configuration
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.etrusted.android.trustbadge.library.R
+import com.etrusted.android.trustbadge.library.common.internal.ExcludeFromJacocoGeneratedReport
+import com.etrusted.android.trustbadge.library.ui.card.protection.ActiveProductListText
+import com.etrusted.android.trustbadge.library.ui.card.protection.TermsAndConditionsAndPrivacyPolicyText
+import com.etrusted.android.trustbadge.library.ui.common.ActionButton
+import com.etrusted.android.trustbadge.library.ui.common.openLinkInExternalBrowser
+import com.etrusted.android.trustbadge.library.ui.theme.TrustbadgeTheme
+import com.etrusted.android.trustbadge.library.ui.theme.TsTextBase
+import com.etrusted.android.trustbadge.library.ui.theme.mobileBody
+import com.etrusted.android.trustbadge.library.ui.theme.mobileCaption
+import com.etrusted.android.trustbadge.library.ui.theme.mobileTermsAndConditions
+
+@Composable
+fun TrustcardProtection(
+    modifier: Modifier = Modifier,
+    orderAmount: String,
+    onClickProtectPurchase: () -> Unit = {},
+    onClickDismiss: () -> Unit = {},
+) {
+
+    val context = LocalContext.current
+    val imprintLink = stringResource(id = R.string.tcard_link_imprint)
+
+    val onClickImprint = {
+        openLinkInExternalBrowser(
+            context = context,
+            link = imprintLink,
+        )
+    }
+
+    TrustcardContainer(
+        modifier = modifier,
+        headingText = stringResource(id = R.string.tcard_t_protect_your_purchase),
+        content = {
+
+            // 1. shop details and order amount
+            Row(modifier = Modifier.padding(start = 16.dp, end = 16.dp)) {
+                Column(Modifier.weight(1f)) {
+                    Text(
+                        style = MaterialTheme.typography.mobileCaption,
+                        color = MaterialTheme.colorScheme.TsTextBase,
+                        text = stringResource(id = R.string.tcard_t_shop)
+                    )
+                    Text(
+                        style = MaterialTheme.typography.mobileBody,
+                        color = MaterialTheme.colorScheme.TsTextBase,
+                        text = "shop-name.com")
+                }
+                Box(modifier = Modifier.weight(1f)) {
+                    Column(modifier = Modifier.align(Alignment.CenterEnd)) {
+                        Text(
+                            style = MaterialTheme.typography.mobileCaption,
+                            color = MaterialTheme.colorScheme.TsTextBase,
+                            text = stringResource(id = R.string.tcard_t_order_amount)
+                        )
+                        Text(
+                            style = MaterialTheme.typography.mobileBody,
+                            color = MaterialTheme.colorScheme.TsTextBase,
+                            text = orderAmount)
+                    }
+                }
+            }
+
+            // 2. active product list and terms and conditions
+            Column(modifier = Modifier
+                .padding(16.dp)
+            ) {
+                ActiveProductListText()
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                TermsAndConditionsAndPrivacyPolicyText()
+            }
+
+            // 3. protect your purchase button
+            ActionButton(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                onClick = onClickProtectPurchase,
+            ) {
+                Text(text = stringResource(id = R.string.tcard_bt_protect_your_purchase))
+            }
+        },
+        footer = {
+            Text(
+                modifier = Modifier
+                    .clickable(enabled = true, onClick = onClickImprint),
+                style = MaterialTheme.typography.mobileTermsAndConditions,
+                text = stringResource(id = R.string.tcard_t_imprint),
+            )
+        },
+        onClickDismiss = onClickDismiss,
+    )
+}
+
+@ExcludeFromJacocoGeneratedReport
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Composable
+fun TrustcardProtectionPreview() {
+    TrustbadgeTheme {
+        TrustcardProtection(
+            orderAmount = "€ 100",
+        )
+    }
+}
+
+@ExcludeFromJacocoGeneratedReport
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun TrustcardProtectionPreviewNight() {
+    TrustbadgeTheme {
+        TrustcardProtection(
+            orderAmount = "€ 100",
+        )
+    }
+}
